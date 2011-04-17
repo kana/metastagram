f0216a34ffc3fd88a9e49babb017c50d7c73c675
kana <nicht@s8.xrea.com>
2011-04-17 21:59:25 +0900

    Add spec for Artist.create

--- spec/javascripts/ArtistSpec.js
+++ spec/javascripts/ArtistSpec.js
@@ -1,4 +1,21 @@
 describe('Artist', function () {
+  describe('create', function () {
+    it('should return a new artist', function () {
+      var a = Metastagram.Artist.create();
+      expect(typeof a.start).toBe('function');
+    });
+    it('should return a new artist with given options', function () {
+      var options = {
+        hotBoxes: $([]),
+        normalBoxes: $([]),
+        updateInterval: 1234
+      };
+      var a = Metastagram.Artist.create(options);
+      for (var key in options) {
+        expect(a.options[key]).toBe(options[key]);
+      }
+    });
+  });
 });
 
 
