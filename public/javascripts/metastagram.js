1c0b6768157326e2d909925419f792299f2657f8
kana <nicht@s8.xrea.com>
2011-04-17 22:16:14 +0900

    Refactor Metastagram - Remove extra quotes

--- public/javascripts/metastagram.js
+++ public/javascripts/metastagram.js
@@ -1,44 +1,44 @@
 var Metastagram = (function (M, $) {
   return $.extend(M, {
-    'Artist': {
-      'create': function (newOptions) {
+    Artist: {
+      create: function (newOptions) {
         var defaultOptions = {
-          'hotBoxes': $([]),
-          'normalBoxes': $([]),
-          'updateInterval': 3000
+          hotBoxes: $([]),
+          normalBoxes: $([]),
+          updateInterval: 3000
         };
         var me = {};
         return $.extend(me, {
-          'options': $.extend({}, defaultOptions, newOptions),
-          'perform': function () {
+          options: $.extend({}, defaultOptions, newOptions),
+          perform: function () {
             // TODO: Refresh boxes in nice order.
             me.refresh(me.options.normalBoxes);
             me.refresh(me.options.hotBoxes);
           },
-          'refresh': function (boxes) {
+          refresh: function (boxes) {
             // TODO: Add nice effect to refresh a box.
             // TODO: Use images from Librarian.
             var box = M.Maid.choose(boxes);
             box.text(M.Maid.random(0, 10));
           },
-          'start': function () {
+          start: function () {
             if (!me.timer)
               me.timer = setInterval(me.perform, me.options.updateInterval);
           },
-          'timer': null
+          timer: null
         });
       },
     },
-    'Explorer': {
+    Explorer: {
     },
-    'Librarian': {
+    Librarian: {
     },
-    'Maid': {
-      'choose': function (array) {
+    Maid: {
+      choose: function (array) {
         var n = M.Maid.random(0, array.length);
         return array.slice(n, n + 1);
       },
-      'random': function (minimum, maximum) {
+      random: function (minimum, maximum) {
         return Math.floor(Math.random() * (maximum - minimum)) + minimum;
       }
     }
