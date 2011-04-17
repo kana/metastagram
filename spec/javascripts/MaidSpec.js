45c9d023ab8b57ef41621b22154dc9958591ca4b
kana <nicht@s8.xrea.com>
2011-04-17 21:43:01 +0900

    Add spec for Maid.random

--- spec/javascripts/MaidSpec.js
+++ spec/javascripts/MaidSpec.js
@@ -1,4 +1,17 @@
 describe('Maid', function () {
+  describe('random', function () {
+    var trialCount = 2011;
+    var minimum = 4;
+    var maximum = 17;
+    it('should return a random integer in [minimum, maximum)', function () {
+      for (var i = 0; i < trialCount; i++) {
+        var n = Metastagram.Maid.random(minimum, maximum);
+        expect(n).toEqual(Math.floor(n));
+        expect(n).not.toBeLessThan(minimum);
+        expect(n).toBeLessThan(maximum);
+      }
+    });
+  });
 });
 
 
